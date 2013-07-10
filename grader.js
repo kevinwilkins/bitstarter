11274c3388f1fe89bb77c5afb7303020592190d1
kevinwilkins <kevin.wilkins@k2photo.co.uk>
2013-07-10 03:42:38 +0100

    updated grader.js

--- grader.js
+++ grader.js
@@ -22,10 +22,13 @@ References:
 */
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLURL_DEFAULT = "";
+var htmlURL = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +68,23 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_file>', 'URL to index.html', htmlURL, HTMLURL_DEFAULT)
         .parse(process.argv);
+if (program.url == ""){
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    fs.writeFileSync("JSONout_file.txt", outJson);
+}
+else{
+    rest.get(program.url).on('complete', function(data) {
+	fs.writeFileSync("live.html", data);
+        var checkJsonUrl = checkHtmlFile("live.html", program.checks);
+        var outJsonUrl = JSON.stringify(checkJsonUrl, null, 4);
+        console.log(outJsonUrl);
+	fs.writeFileSync("JSONout_html.txt", outJsonUrl);
+    });
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
